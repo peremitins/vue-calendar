917989fb7587a72c049e461cdfa318109b4214f0
nikolay_peremitin <peremitinns@mail.ru>
2021-11-02 20:06:00 +0300

    create vue-calendar with firebase and vuetify

--- README.md
+++ README.md
@@ -1,24 +1,20 @@
 # vue-calendar
 
+##### https://allfreelance.github.io/vue-calendar/
+
+> Implementing a full calendar by adding via google Firestore.<br>
+> Used HTML, CSS/SCSS, JS, Vuejs, Git, Firestore.
+
+[![](https://github.com/allfreelance/vue-calendar/blob/main/screen.jpg)](https://allfreelance.github.io/vue-calendar/)
+
 ## Project setup
-```
+
 npm install
-```
 
 ### Compiles and hot-reloads for development
-```
+
 npm run serve
-```
 
 ### Compiles and minifies for production
-```
+
 npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import vuetify from './plugins/vuetify';
+import VueTextareaAutosize from 'vue-textarea-autosize';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
+import 'firebase/firestore';
 
-Vue.config.productionTip = false
+Vue.use(VueTextareaAutosize);
+
+Vue.config.productionTip = false;
+
+initializeApp({
+  apiKey: "AIzaSyAy3kAlAcaM_2kH4uegiQ686qcJi5WQeQU",
+  authDomain: "vue-calendar-e90b9.firebaseapp.com",
+  projectId: "vue-calendar-e90b9",
+  storageBucket: "vue-calendar-e90b9.appspot.com",
+  messagingSenderId: "702125211183",
+  appId: "1:702125211183:web:5f902a203314c9094950da"
+});
+
+export const db = getFirestore();
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
